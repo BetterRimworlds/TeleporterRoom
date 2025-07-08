c2df6d939ee6daa6b21e7c320ab25ca920916469
Theodore R. Smith <hopeseekr@gmail.com>
2025-07-08 08:58:41 -0500

    Migrated to a modern dotnet SDK project.

--- build.sh
+++ build.sh
@@ -17,14 +17,29 @@ solutionPath="Source/${MOD}.sln"
 # Define an array of configurations
 configurations=("Release v1.2" "Release v1.3" "Release v1.4" "Release v1.5")
 
+dotnet restore "$solutionPath"
+
 function build() {
+    rm -rf /rimworld/1.2/Mods/${MOD}
+
     # Loop through each configuration and build it
     for config in "${configurations[@]}"; do
         echo "Building for configuration: $config"
-        dotnet msbuild "$solutionPath" /p:Configuration="$config" &
+        dotnet build --no-restore "$solutionPath" --configuration "$config" &
     done
 
     wait  # Blocks until all background jobs finish
+
+    # Copy over the mod directory.
+    rsync -a ${MOD} /rimworld/1.2/Mods/
+
+    # Copy over and reformat the README.
+    cp README.md /rimworld/1.2/Mods/${MOD}
+    unix2dos /rimworld/1.2/Mods/${MOD}/README.md
+
+    rm -rf /rimworld/1.3/Mods/${MOD}
+    rm -rf /rimworld/1.4/Mods/${MOD}
+    rm -rf /rimworld/1.5/Mods/${MOD}
 
     cp -af /rimworld/1.2/Mods/${MOD} /rimworld/1.3/Mods
     cp -af /rimworld/1.2/Mods/${MOD} /rimworld/1.4/Mods
@@ -48,4 +63,3 @@ inotifywait --recursive --monitor --format "%e %w%f" \
         echo "Detected change in $changed"
         build
     done
-
